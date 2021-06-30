b8b7af0a184b9f47592addbc023af131213851ec
katsuya <katsuya02087@icloud.com>
2021-06-30 23:30:30 +0900

    検索機能実装

--- TWITTERCLONE/Models/tweets.php
+++ TWITTERCLONE/Models/tweets.php
@@ -42,9 +42,10 @@ function createTweet(array $data)
  * ツイート一覧を取得
  *
  * @param array $user ログインしているユーザー情報
+ * @param string $keyword 検索キーワード
  * @return array|false
  */
-function findTweets(array $user)
+function findTweets(array $user, string $keyword = null)
 {
     $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
     // 接続チェック
@@ -83,6 +84,19 @@ function findTweets(array $user)
         WHERE
             T.status = 'active'
     SQL;
+ 
+    // キーワードが入力されていた場合
+    if (isset($keyword)) {
+        // エスケープ
+        $keyword = $mysqli->real_escape_string($keyword);
+        // ツイート主のニックネーム・ユーザー名・本文から部分一致検索
+        $query .= ' AND CONCAT(U.nickname, U.name, T.body) LIKE "%' . $keyword . '%"';
+    }
+ 
+    // 新しい順に並び替え
+    $query .= ' ORDER BY T.created_at DESC';
+    // 50件表示
+    $query .= ' LIMIT 50';
  
     // SQL実行
     if ($result = $mysqli->query($query)) {
