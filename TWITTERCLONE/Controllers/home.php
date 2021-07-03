c15eb14bd76e74f9d6447f1466021d68b083e03d
katsuya <katsuya02087@icloud.com>
2021-07-03 16:08:00 +0900

    ホーム画面をタイムライン化実装

--- TWITTERCLONE/Controllers/home.php
+++ TWITTERCLONE/Controllers/home.php
@@ -10,6 +10,8 @@ include_once '../util.php';
  
 // ツイートデータ操作モデルを読み込む
 include_once '../Models/tweets.php';
+// フォローデータ操作モデルを読み込む
+include_once '../Models/follows.php';
  
 // ログインしているか
 $user = getUserSession();
@@ -19,8 +21,13 @@ if (!$user) {
     exit;
 }
  
+// 自分がフォローしているユーザーID一覧を取得
+$following_user_ids = findFollowingUserIds($user['id']);
+// 自分のツイートも表示するために自分のIDも追加
+$following_user_ids[] = $user['id'];
+ 
 // 画面表示
 $view_user = $user;
 // ツイート一覧
-$view_tweets = findTweets($user);
+$view_tweets = findTweets($user, null, $following_user_ids);
 include_once '../Views/home.php';
--- TWITTERCLONE/Models/follows.php
+++ TWITTERCLONE/Models/follows.php
@@ -81,3 +81,49 @@ function deleteFollow(array $data)
  
     return $response;
 }
+ 
+/**
+ * 自分がフォーローしているユーザーID一覧を取得
+ *
+ * @param int $follow_user_id
+ * @return array|false
+ */
+function findFollowingUserIds(int $follow_user_id)
+{
+    $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+    // 接続チェック
+    if ($mysqli->connect_errno) {
+        echo 'MySQLの接続に失敗しました。：' . $mysqli->connect_error . "\n";
+        exit;
+    }
+ 
+    // エスケープ
+    $follow_user_id = $mysqli->real_escape_string($follow_user_id);
+ 
+    // 検索のSQLを作成
+    $query = 'SELECT followed_user_id FROM follows'
+        . ' WHERE status = "active" AND follow_user_id ="' . $follow_user_id . '"';
+ 
+    // SQL実行
+    $result = $mysqli->query($query);
+    if (!$result) {
+        echo 'エラーメッセージ：' . $mysqli->error . "\n";
+        // 接続を閉じる
+        $mysqli->close();
+        return false;
+    }
+ 
+    // フォロー一覧を取得
+    $follows = $result->fetch_all(MYSQLI_ASSOC);
+ 
+    // ユーザーIDの一覧を作成
+    $following_user_ids = [];
+    foreach ($follows as $follow) {
+        $following_user_ids[] = $follow['followed_user_id'];
+    }
+ 
+    // 接続を閉じる
+    $mysqli->close();
+ 
+    return $following_user_ids;
+}
